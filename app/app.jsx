6a75b3f646d58331d9c65be8e32375fc82db8a93
papascott <papascott@gmail.com>
2016-05-26 09:55:20 +0200

    start ToDoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var ToDoApp = require('ToDoApp');
+
 // load foundation
 //require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
@@ -10,6 +12,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <ToDoApp/>,
   document.getElementById('app')
 );
