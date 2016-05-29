a7dfc325c857244257224c1ad3fb5510909e3c93
papascott <papascott@gmail.com>
2016-05-29 18:48:16 +0200

    correct TodoSearch.jsx

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -1,21 +1,21 @@
 var React = require('react');
 
-var TodoSearch = React.createClass ({
-  handleSubmit: function () {
+var TodoSearch = React.createClass({
+  handleSearch: function () {
     var showCompleted = this.refs.showCompleted.checked;
     var searchText = this.refs.searchText.value;
 
-    this.props.onSearch(showCompleted,searchText);
+    this.props.onSearch(showCompleted, searchText);
   },
   render: function () {
     return (
       <div>
         <div>
-          <input type="search" ref="searchText" placeholder="Seach todos" onChange={this.handleSearch}/>
+          <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
         </div>
         <div>
           <label>
-            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch} />
+            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
             Show completed todos
           </label>
         </div>
