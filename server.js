aced38af69b15564d9314baa42becf23c684032e
papascott <papascott@gmail.com>
2016-08-16 08:49:52 +0200

    rename TodoApp.jsx

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
@@ -13,7 +13,6 @@ app.use(function (req, res, next){
 });
 
 app.use(express.static('public'));
-
 
 app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
